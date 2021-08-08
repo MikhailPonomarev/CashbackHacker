fee663f757d0710d10f5290b9a3eb44c6656adce
Mikhail Ponomarev <michailp800@gmail.com>
2021-08-08 20:33:21 +0300

    modified gradle to use TestNG, tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void over1000() {
+        assertEquals(service.remain(1001),999);
+    }
+
+    @Test
+    public void below1000() {
+        assertEquals(service.remain(999),1);
+    }
+
+    @Test
+    public void ifAmountZero() {
+        assertEquals(service.remain(0),1000);
+    }
+
+    @Test
+    public void ifAmountNegative() {
+        assertEquals(service.remain(-1),1001);
+    }
+}
