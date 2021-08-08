c37134acaffe54a8f5d6e63a5e3b67a4f64f2214
Mikhail Ponomarev <michailp800@gmail.com>
2021-08-08 21:19:06 +0300

    added: junit jupiter dependencies, tests using junit jupiter API

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.service;
 import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -30,6 +31,31 @@ public class CashbackHackServiceTest {
     @Test
     public void buyEquals1000() {
         Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void buyOver1000 () {
+        Assertions.assertEquals(999, service.remain(1001));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void buyBelow1000 () {
+        Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void buyEqualsZero() {
+        Assertions.assertEquals(1000, service.remain(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ifBuyEqualsNegative() {
+        Assertions.assertEquals(1001, service.remain(-1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ifBuyEquals1000() {
+        Assertions.assertEquals(0, service.remain(1000));
     }
 
 }
